549de9d69ae6f9f7a98c1f93ee42dff32e880465
copier <baigrais13@gmail.com>
2023-12-04 14:21:18 +0500

    made a connection with a SQL database and deleted the config file

---
+++ config.py
@@ -0,0 +1,7 @@
+# This is a configuration file
+
+import os
+
+Server=DESKTOP-B6KS6C8
+Database=os. DW
+Trusted_Connection=yes
--- util.py
+++ util.py
@@ -1,3 +1,24 @@
 # This is a util file where the connections will be performed
 import pyodbc
-import config
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+
+def connection_to_db():
+
+    Server = os.getenv("Server")
+    DB = os.getenv("Database")
+    Trusted_Connection = os.getenv("Trusted_Connection")
+
+    connection_string = f"Driver={{ODBC Driver 17 for SQL Server}};Server={Server};Database={DB};Trusted_Connection={Trusted_Connection};"
+    
+    try:
+        connection = pyodbc.connect(connection_string)
+        print("Connection Successfull")
+    except pyodbc.Error as e:
+        print(f"Error connecting to SQL Server : {e}")
+    except Exception as e:
+        print(f"An unexpected error occured : {e}")
+    return connection
