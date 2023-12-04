f4db3f165ef90ba29dfa15f7719bf5d33a0ee1e0
copier <baigrais13@gmail.com>
2023-12-05 02:58:45 +0500

    read data from csv's and convert them into dataframes and then checked the connection with the database by executing select and insert queries

--- execute.py
+++ execute.py
@@ -1,3 +1,55 @@
 import util
+import os
+import files
 
-util
+connex = util.connection_to_db()
+cursor = connex.cursor()
+query1 = """
+        INSERT INTO [dbo].[order_details]
+           ([order_details_id]
+           ,[order_id]
+           ,[order_date]
+           ,[order_time]
+           ,[item_id]) 
+        values(12236, 5372, getdate(), '11:11:11 AM', 161.0)
+    """
+query = """
+        select * from [dbo].[order_details]
+    """
+# print(query)
+cursor.execute(query1)
+connex.commit()
+cursor.close()
+connex.close()
+for val in cursor:
+    print(val)
+
+
+# cursor.execute("select * from order_details;")
+# for i in cursor:
+#     print(i)
+
+# def bulkInsert(filename):
+
+#     query = f"""
+#             BULK INSERT {tablename}
+#             from '{filename}'
+#             WITH
+#             (
+#             FORMAT = 'CSV',
+#             FIRSTROW = 2,
+#             FILEDTERMINATOR = ',',
+#             ROWTERMINATOR = '\\n'
+#             )
+#         """
+#     return query
+
+
+
+
+# def getRecords(filename):
+#     pass
+
+
+# def insertRecords(tablename):
+#     pass
--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 import os
 import pandas as pd
-import util
 import files
 
 
 
 def main():
     datafile_folder = os.path.join("G:\My Drive\Desktop Files\DataSets for Visualizations\DateSets_2Dec2023\Restaurant\Restaurant+Orders+CSV")
-    file = files.get_file(datafile_folder)
-    print(file)
-
-def bulkInsert(filename, tablename):
-
-    query = f"""
-            BULK INSERT {tablename}
-            from '{filename}'
-            WITH
-            (
-            FORMAT = 'CSV',
-            FIRSTROW = 2,
-            FILEDTERMINATOR = ',',
-            ROWTERMINATOR = '\\n'
-            )
-        """
-    return query
-
-
-def getRecords(filename):
-    pass
-
-
-def insertRecords(tablename):
-    pass
-
+    csv_files = files.get_file(datafile_folder)
+    dataframes = []
+    for file in csv_files:
+        # print(file)
+        if file == 'order_details.csv':
+            order_details = pd.read_csv(f"{datafile_folder}\{file}")
+            order_details['order_date'] = pd.to_datetime(order_details['order_date'], format="%d/%m/%Y")
+            # print(order_details)
+            dataframes.append(order_details)
+        if file == 'menu_items.csv':
+            menu_items = pd.read_csv(f"{datafile_folder}\{file}")
+            # print("-----------------")
+            # print(menu_items.head(5))
+            dataframes.append(menu_items)
 
 if __name__ == "__main__":
     main()
