29ca21860c26ab2e16dc1d35b2091735f8253b7a
copier <baigrais13@gmail.com>
2023-12-05 21:18:01 +0500

    written code to get a dynamic query to insert data and a function to insert data into tables

--- execute.py
+++ execute.py
@@ -1,55 +1,30 @@
 import util
-import os
-import files
-
-connex = util.connection_to_db()
-cursor = connex.cursor()
-query1 = """
-        INSERT INTO [dbo].[order_details]
-           ([order_details_id]
-           ,[order_id]
-           ,[order_date]
-           ,[order_time]
-           ,[item_id]) 
-        values(12236, 5372, getdate(), '11:11:11 AM', 161.0)
-    """
-query = """
-        select * from [dbo].[order_details]
-    """
-# print(query)
-cursor.execute(query1)
-connex.commit()
-cursor.close()
-connex.close()
-for val in cursor:
-    print(val)
 
 
-# cursor.execute("select * from order_details;")
-# for i in cursor:
-#     print(i)
+def insertQuery(df, file):
+    tablename = file.split('.')[0]
+    column_names = list(df.columns.values)
+    column_names_string = ', '.join(column_names)
+    column_values = tuple(map(lambda column: column.replace(column, '?'), column_names))
+    column_values_string = ', '.join(column_values)
+    
+    query = f"""
+                INSERT INTO {tablename} ({column_names_string}) VALUES ({column_values_string})
+    
+            """
+    
+    return query
 
-# def bulkInsert(filename):
+def dataLoad(dataframe, order_details, file):
+    query = insertQuery(order_details, file)
 
-#     query = f"""
-#             BULK INSERT {tablename}
-#             from '{filename}'
-#             WITH
-#             (
-#             FORMAT = 'CSV',
-#             FIRSTROW = 2,
-#             FILEDTERMINATOR = ',',
-#             ROWTERMINATOR = '\\n'
-#             )
-#         """
-#     return query
-
-
-
-
-# def getRecords(filename):
-#     pass
-
-
-# def insertRecords(tablename):
-#     pass
+    connex = util.connection_to_db()
+    cursor = connex.cursor()
+    
+    print(query)
+    for index, row in dataframe.iterrows():
+        cursor.execute(query, row.order_details_id, row.order_id, row.order_date, row.order_time, row.item_id)
+    connex.commit()
+    cursor.close()
+    connex.close()
+    
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import files
+import execute
 
 
 
@@ -9,17 +10,11 @@ def main():
     csv_files = files.get_file(datafile_folder)
     dataframes = []
     for file in csv_files:
-        # print(file)
         if file == 'order_details.csv':
             order_details = pd.read_csv(f"{datafile_folder}\{file}")
-            order_details['order_date'] = pd.to_datetime(order_details['order_date'], format="%d/%m/%Y")
-            # print(order_details)
+            # order_details['order_date'] = pd.to_datetime(order_details['order_date'])
+            execute.dataLoad(order_details, order_details, file)
             dataframes.append(order_details)
-        if file == 'menu_items.csv':
-            menu_items = pd.read_csv(f"{datafile_folder}\{file}")
-            # print("-----------------")
-            # print(menu_items.head(5))
-            dataframes.append(menu_items)
 
 if __name__ == "__main__":
     main()
