f7bb49a0721300afc5bdc673beb3542f56804945
copier <baigrais13@gmail.com>
2023-12-06 17:14:26 +0500

    freezed the requirements.txt file and modified the code that load data into the sql server tables

--- execute.py
+++ execute.py
@@ -1,30 +1,42 @@
 import util
 
-
 def insertQuery(df, file):
     tablename = file.split('.')[0]
     column_names = list(df.columns.values)
     column_names_string = ', '.join(column_names)
     column_values = tuple(map(lambda column: column.replace(column, '?'), column_names))
     column_values_string = ', '.join(column_values)
+    column_to_insert = ', '.join([f"row['{column}']" for column in column_names])
     
     query = f"""
-                INSERT INTO {tablename} ({column_names_string}) VALUES ({column_values_string})
+            INSERT INTO {tablename} ({column_names_string}) VALUES ({column_values_string})
     
             """
     
-    return query
+    return query, column_to_insert
 
-def dataLoad(dataframe, order_details, file):
-    query = insertQuery(order_details, file)
-
+def dataLoad(dataframe, file):
+    query, column_to_insert = insertQuery(dataframe, file)
     connex = util.connection_to_db()
     cursor = connex.cursor()
-    
-    print(query)
+    row_processed = 0
     for index, row in dataframe.iterrows():
-        cursor.execute(query, row.order_details_id, row.order_id, row.order_date, row.order_time, row.item_id)
+        print(row)
+        row_processed = row_processed+1
+        cursor.execute(query+column_to_insert)
+        try:
+            
+            cursor.execute(f"""
+                            INSERT INTO order_details ([order_details_id],
+                        [order_id]
+                        ,[order_date]
+                        ,[order_time]
+                        ,[item_id]) VALUES (?,?,?,?,?)
+                            """
+                        , row['order_details_id'], row['order_id'], row['order_date'], row['order_time'], row['item_id']
+                        )
+        except Exception as e:
+            print("e", e)
     connex.commit()
     cursor.close()
     connex.close()
-    
--- main.py
+++ main.py
@@ -2,19 +2,65 @@ import os
 import pandas as pd
 import files
 import execute
-
+import util
 
 
 def main():
     datafile_folder = os.path.join("G:\My Drive\Desktop Files\DataSets for Visualizations\DateSets_2Dec2023\Restaurant\Restaurant+Orders+CSV")
     csv_files = files.get_file(datafile_folder)
-    dataframes = []
+    
     for file in csv_files:
         if file == 'order_details.csv':
             order_details = pd.read_csv(f"{datafile_folder}\{file}")
-            # order_details['order_date'] = pd.to_datetime(order_details['order_date'])
-            execute.dataLoad(order_details, order_details, file)
-            dataframes.append(order_details)
+            order_details['order_date'] = pd.to_datetime(order_details['order_date'])
+            order_details['item_id'] = order_details['item_id'].fillna(0).astype(int)
+            order_details.fillna("", inplace=True)
+            connex = util.connection_to_db()
+            cursor = connex.cursor()
+            row_processed = 0
+            for index, row in order_details.iterrows():
+                row_processed = row_processed+1
+                try:
+                    cursor.execute("""
+                                    INSERT INTO order_details ([order_details_id],
+                                [order_id]
+                                ,[order_date]
+                                ,[order_time]
+                                ,[item_id]) VALUES (?,?,?,?,?)
+                                    """
+                                , row['order_details_id'], row['order_id'], row['order_date'], row['order_time'], row['item_id']
+                                )
+                except Exception as e:
+                    print("e", e)
+            connex.commit()
+            cursor.close()
+            connex.close()
+        if file == 'menu_items.csv':
+            menu_items = pd.read_csv(f"{datafile_folder}\{file}")
+            menu_items.fillna("", inplace=True)
+            connex = util.connection_to_db()
+            cursor = connex.cursor()
+            row_processed = 0
+            for index, row in menu_items.iterrows():
+                row_processed = row_processed+1
+                try:
+                    cursor.execute("""
+                                    INSERT INTO menu_items (
+                                    [menu_item_id]
+                                    ,[item_name]
+                                    ,[category]
+                                    ,[price])
+                                    VALUES (?,?,?,?)
+                                    """
+                                , row['menu_item_id'], row['item_name'], row['category'], row['price']
+                                )
+                except Exception as e:
+                    print("e", e)
+            connex.commit()
+            cursor.close()
+            connex.close()
+
+
 
 if __name__ == "__main__":
     main()
